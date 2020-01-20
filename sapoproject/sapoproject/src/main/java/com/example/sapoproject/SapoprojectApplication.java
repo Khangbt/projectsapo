d1f03a3919914c5309cdb943bacc3c0d688d13c1
khangbt <buithekhang246@gmail.com>
2020-01-20 17:36:37 +0700

    tết

---
+++ sapoproject/sapoproject/src/main/java/com/example/sapoproject/SapoprojectApplication.java
@@ -0,0 +1,18 @@
+package com.example.sapoproject;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapoprojectApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapoprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ssssssssss");
+	}
+}
